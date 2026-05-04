167c46f20cc3eb0ed7e8c984f682c65539062454
axdx-luke <tanluc.dev@gmail.com>
2026-05-04 23:35:45 +0700

    Chore: Update error 500

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -4,24 +4,39 @@ import {
   ArgumentsHost,
   HttpException,
   HttpStatus,
+  Logger,
 } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 
 @Catch()
 export class GlobalExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(GlobalExceptionFilter.name);
+
   catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
     const request = ctx.getRequest();
 
-    const status =
-      exception instanceof HttpException
-        ? exception.getStatus()
-        : HttpStatus.INTERNAL_SERVER_ERROR;
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message: unknown = 'Lỗi server, vui lòng thử lại';
 
-    const message =
-      exception instanceof HttpException
-        ? exception.getResponse()
-        : 'Lỗi server, vui lòng thử lại';
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      message = exception.getResponse();
+    } else if (exception instanceof Prisma.PrismaClientKnownRequestError) {
+      this.logger.error(`Prisma error ${exception.code} on ${request.url}`, exception.message);
+      if (exception.code === 'P2002') {
+        status = HttpStatus.CONFLICT;
+        message = 'Dữ liệu đã tồn tại';
+      } else if (exception.code === 'P2025') {
+        status = HttpStatus.NOT_FOUND;
+        message = 'Không tìm thấy dữ liệu';
+      }
+    } else if (exception instanceof Prisma.PrismaClientInitializationError) {
+      this.logger.error(`Prisma init error on ${request.url}`, (exception as Error).message);
+    } else {
+      this.logger.error(`Unhandled exception on ${request.url}`, exception);
+    }
 
     response.status(status).json({
       statusCode: status,
